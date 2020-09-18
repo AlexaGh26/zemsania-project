2cf2ce8acb9b6bff45fc520382a0b10e76277bdb
Alexagh26 <alexagh26@gmail.com>
2020-09-17 21:55:46 -0500

    add table PrimeNg

--- project/src/app/page/home/home.component.ts
+++ project/src/app/page/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Customer } from '../../../app/domain/customers';
+import { CustomerService } from '../../services/customerservice';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +9,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+    customers: Customer[];
 
-  ngOnInit(): void {
-  }
+    first = 0;
+
+    rows = 10;
+
+    constructor(private customerService: CustomerService) { }
+
+    ngOnInit() {
+        this.customerService.getCustomersLarge().then(customers => this.customers = customers);
+    }
+
+    next() {
+        this.first = this.first + this.rows;
+    }
+
+    prev() {
+        this.first = this.first - this.rows;
+    }
+
+    reset() {
+        this.first = 0;
+    }
+
+    isLastPage(): boolean {
+        return this.customers ? this.first === (this.customers.length - this.rows): true;
+    }
+
+    isFirstPage(): boolean {
+        return this.customers ? this.first === 0 : true;
+    }
 
 }
--- project/src/app/page/home/home.module.ts
+++ project/src/app/page/home/home.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
-import {HomeComponent} from './home.component'
+import {HomeComponent} from './home.component';
+import {TableModule} from 'primeng/table';
 
 
 @NgModule({
   declarations: [HomeComponent],
   exports: [HomeComponent],
   imports: [
+    TableModule,
     ]
 })
 export class HomeModule { }
